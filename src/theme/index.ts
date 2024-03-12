0225f953000ac404c9703156e25f9533f7c9a0e2
MikeFernando <mikera2020@outlook.com>
2024-03-12 14:41:16 -0300

    first commit

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+
+    GREEN_DARK: "#639339",
+    GREEN_MID: "#CBE4B4",
+    GREEN_LIGHT: "#E5F0DB",
+
+    RED_DARK: "#BF3B44",
+    RED_MID: "#F3BABD",
+    RED_LIGHT: "#F4E6E7",
+
+    GRAY_700: "#1B1D1E",
+    GRAY_600: "#333638",
+    GRAY_500: "#5C6265",
+    GRAY_400: "#B9BBBC",
+    GRAY_300: "#DDDEDF",
+    GRAY_200: "#EFF0F0",
+    GRAY_100: "#FAFAFA",
+  },
+  FONT_FAMILY: {
+    REGULAR: "Roboto_400Regular",
+    BOLD: "Roboto_700Bold",
+  },
+  FONT_SIZE: {
+    XM: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32,
+  },
+};
